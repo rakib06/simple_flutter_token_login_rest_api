6095697452c5fa1ea002ee71acb84ad540267dd8
Rakibul Hasan <six.rakib@gmail.com>
2021-02-26 11:43:19 +0600

    simple login with rest api done

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:exam_player/model/user_model.dart';
+import 'package:exam_player/model/api_model.dart';
 
-final _base = "http://127.0.0.1:8000";
-final _tokenEndpoint = "/api-token-auth/";
+final _base = "http://10.0.2.2:8000";
+final _tokenEndpoint = "/api-token-auth";
 final _tokenURL = _base + _tokenEndpoint;
 
 Future<Token> getToken(UserLogin userLogin) async {
@@ -16,4 +16,10 @@ Future<Token> getToken(UserLogin userLogin) async {
     },
     body: jsonEncode(userLogin.toDatabaseJson()),
   );
+  if (response.statusCode == 200) {
+    return Token.fromJson(json.decode(response.body));
+  } else {
+    print(json.decode(response.body).toString());
+    throw Exception(json.decode(response.body));
+  }
 }
